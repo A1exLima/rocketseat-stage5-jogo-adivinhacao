7ab62e4840ed62a973da86fabd10ef027c99ef0d
Alima07 <alex.lima_dev@outlook.com>
2023-02-15 21:37:15 -0300

    Lógica mensagem de tentativa

--- public/javaScript/main.js
+++ public/javaScript/main.js
@@ -5,8 +5,22 @@ const screen2 = document.querySelector(".screen2")
 const btnTry = document.querySelector(".button-try")
 const btnReload = document.querySelector(".button-again")
 //Game logic
+const tentativeMessage = document.querySelector(".tentative-message")
 let randomNumber = Math.round(Math.random() * 10)
 let xAttempts = 1
+let arrayMessage = [
+  "Quase acertou, tente mais uma vez 🙌",
+  "Não foi dessa vez, continue tentando 🥲",
+  "Na próxima você acerta 👌",
+  "Faltou pouco, tente novamente 😝",
+  "Não desista, está quase lá 🏃‍♂️",
+  "Faltou um fio de cabelo para acertar 😣",
+  "Está quase, mais uma vez 😗",
+  "Tente novamente 😉",
+  "Mais algumas tentativas você consegue 😊",
+  "Quase profissional, continue tentando 👍",
+  "Persista mais um pouco, mais uma vez 😲",
+]
 
 //Events
 btnTry.addEventListener("click", handleTryClick)
@@ -19,20 +33,25 @@ function handleTryClick(event) {
 
   const inputGuess = document.querySelector("#guess")
 
-  if (inputGuess.value > 10 || inputGuess.value < 0) { //Options Gap Validation
+  if (inputGuess.value > 10 || inputGuess.value < 0) {
+    //Options Gap Validation
 
     alert("⚠️  Escolha um número entre 0 e 10  ⚠️")
     --xAttempts
-
   } else if (Number(inputGuess.value) == randomNumber) {
-      toggleScreen() //screen change
+    toggleScreen() //screen change
 
-      document.querySelector(
-        ".screen2 h1"
-      ).innerText = `Acertou em ${xAttempts} tentativas`
-    }
-  
-  if(inputGuess.value != ""){ // If input has a value, it increments the number of attempts, otherwise it does not increment
+    document.querySelector(
+      ".screen2 h1"
+    ).innerText = `Acertou em ${xAttempts} tentativas`
+  } else {
+    let randomArrayMessage = Math.round(Math.random() * 10)
+
+    tentativeMessage.innerText = arrayMessage[randomArrayMessage]
+  }
+
+  if (inputGuess.value != "") {
+    // If input has a value, it increments the number of attempts, otherwise it does not increment
     xAttempts++
   }
 
@@ -45,6 +64,7 @@ function reloadGame(event) {
   toggleScreen() //screen change
   xAttempts = 1 // reset number of attempts
   randomNumber = Math.round(Math.random() * 10) //recalculate random number
+  tentativeMessage.innerText = "" //Clear attempt messages
 }
 
 function toggleScreen() {
